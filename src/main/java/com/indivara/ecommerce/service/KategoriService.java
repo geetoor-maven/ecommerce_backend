0f46218a1bd49189205bc2a50e3169092da0bd81
“Agus <agus40994@gmail.com>
2022-11-01 23:23:48 +0700

    update database with mysql and applicationproperties and add controller

--- src/main/java/com/indivara/ecommerce/service/KategoriService.java
+++ src/main/java/com/indivara/ecommerce/service/KategoriService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class KategoriService {
@@ -23,6 +24,7 @@ public class KategoriService {
     }
 
     public Kategori create(Kategori kategori){
+        kategori.setId(UUID.randomUUID().toString());
         return kategoriRepository.save(kategori);
     }
 
--- src/main/java/com/indivara/ecommerce/service/ProdukService.java
+++ src/main/java/com/indivara/ecommerce/service/ProdukService.java
@@ -1,11 +1,13 @@
 package com.indivara.ecommerce.service;
 
 import com.indivara.ecommerce.entity.Produk;
+import com.indivara.ecommerce.exception.RecourceNotFoundExcep;
 import com.indivara.ecommerce.repository.ProdukRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProdukService {
@@ -17,4 +19,26 @@ public class ProdukService {
         return produkRepository.findAll();
     }
 
+    public Produk findById(String id){
+        return produkRepository.findById(id).orElseThrow(() -> new RecourceNotFoundExcep("Produk dengan id : " + id + " tidak dapat di temukan"));
+    }
+
+    public Produk create(Produk produk){
+        produk.setId(UUID.randomUUID().toString());
+        return produkRepository.save(produk);
+    }
+
+    public Produk edit(Produk produk){
+        return produkRepository.save(produk);
+    }
+
+    public Produk ubahGambar(String id, String gambar){
+        Produk produk = findById(id);
+        produk.setGambar(gambar);
+        return produkRepository.save(produk);
+    }
+
+    public void deleteById(String id){
+        produkRepository.deleteById(id);
+    }
 }
