945f445c01c8dd71f7cdc6a799374c3290c8b6e2
“Agus <agus40994@gmail.com>
2022-11-02 12:14:14 +0700

    add banner in resource file

---
+++ src/main/java/com/indivara/ecommerce/controller/ProdukController.java
@@ -0,0 +1,33 @@
+package com.indivara.ecommerce.controller;
+
+import com.indivara.ecommerce.entity.Produk;
+import com.indivara.ecommerce.service.ProdukService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProdukController {
+
+    @Autowired
+    private ProdukService produkService;
+
+    @GetMapping("/produk")
+    public List<Produk> findAll(){
+        return produkService.findAll();
+    }
+
+    @GetMapping("/produk/{id}")
+    public Produk findById(@PathVariable("id") String id){
+        return produkService.findById(id);
+    }
+
+    @PostMapping("/produk")
+    public Produk create(@RequestBody Produk produk){
+        return produkService.create(produk);
+    }
+
+
+}
